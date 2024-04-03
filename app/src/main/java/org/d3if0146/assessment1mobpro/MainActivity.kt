6989dc8c0be967deb0bb0f305211756c277695f1
Muhammad Daffi Erick Affan <Daffi.affan2709@gmail.com>
2024-04-04 04:02:15 +0700

    Membuat input field

--- app/src/main/java/org/d3if0146/assessment1mobpro/MainActivity.kt
+++ app/src/main/java/org/d3if0146/assessment1mobpro/MainActivity.kt
@@ -1,33 +1,13 @@
 package org.d3if0146.assessment1mobpro
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import org.d3if0146.assessment1mobpro.ui.screen.MainScreen
 import org.d3if0146.assessment1mobpro.ui.theme.Assessment1MobProTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,59 +18,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Counter()
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Counter() {
-    var number by remember { mutableIntStateOf(0) }
-
-    MainScreen { modifier ->
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-        }
-    }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title =
-                {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    ) { padding ->
-        content(Modifier.padding(padding))
-
-    }
-}
-
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assessment1MobProTheme {
-        Counter()
     }
 }
